997ab72b979d9ff8e2884e03f775d873548dee03
Niral Shekhat <120238115+22NiralShekhat@users.noreply.github.com>
2023-06-10 15:38:09 +0530

    Changed port

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express=require('express')
+var express=require('express');
 let encodeUrl=express.urlencoded({extended:false});
 var router=express.Router();
-var app=express()
-var con=require('./databse')
-
+var app=express();
+var con=require('./databse');
+const port=process.env.PORT||8080;
 app.use(encodeUrl);
 app.set('view engine', 'ejs');
 
-var routes=require('./routes')
+var routes=require('./routes');
 
 app.use('/', routes);
 app.use('/pending', routes);
 app.use('/customers', routes);
 
 app.use(router);
-app.listen(8080,(err)=>{
+app.listen(port,(err)=>{
     if (err) throw err;
-    console.log("server running at port 8080")
+    console.log("server running at port 8080");
 });
